cbad00a9c658eaac5ff05d5ac68c0a9b4b95ed7d
Ujjwal <Ujjwalsinghal9837400133@gmail.com>
2021-06-13 18:38:51 +0530

    Simple contact app

---
+++ app/src/main/java/com/example/contactapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.contactapp;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    ListView l1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        l1=(ListView)findViewById(R.id.listView);
+    }
+    public void get (View v){
+        Cursor cursor =  getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null);
+
+        startManagingCursor(cursor);
+        String[] from={ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME ,
+        ContactsContract.CommonDataKinds.Phone.NUMBER,ContactsContract.CommonDataKinds.Phone._ID};
+
+        int[] to ={android.R.id.text1,android.R.id.text2};
+
+
+        SimpleCursorAdapter simpleCursorAdapter= new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2,cursor,from,to);
+
+        l1.setAdapter(simpleCursorAdapter);
+        l1.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+
+    }
+}
